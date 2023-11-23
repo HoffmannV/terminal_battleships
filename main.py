9b67a5480805b4e54778b6acbe6ef5d889b66357
@HoffmannV <vladimir.hoffmann@outlook.de>
2023-11-23 13:25:59 +0100

    Initial Commit - Added logo and the menu options no logic implemented

---
+++ main.py
@@ -0,0 +1,26 @@
+def load_logo():
+    logo = """
+//      ____        __  __  __          __    _                 
+//     / __ )____ _/ /_/ /_/ /__  _____/ /_  (_)___  _____      
+//    / __  / __ `/ __/ __/ / _ \\/ ___/ __ \\/ / __ \\/ ___/   
+//   / /_/ / /_/ / /_/ /_/ /  __(__  ) / / / / /_/ (__  )       
+//  /_____/\\__,_/\\__/\\__/_/\\___/____/_/ /_/_/ .___/____/    
+//                                         /_/                  
+"""
+    print(logo)
+
+
+def load_menu_options():
+    menu_options = """
+//  [S]tart game
+//  [R]esume game
+//  [M]ultiplayer
+//  [E]xit
+"""
+
+    print(menu_options)
+
+
+if __name__ == '__main__':
+    load_logo()
+    load_menu_options()
