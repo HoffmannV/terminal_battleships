09ac4c8b852a8f807bb73f98c0513f10ada6bbe7
@HoffmannV <vladimir.hoffmann@outlook.de>
2023-11-24 13:55:52 +0100

    Created a Game class (draws the grid for the game) also created a class in which the colors for the background are stored. Tested the created functions in main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from Game import Game
+
+
 def load_logo():
     logo = """//      ____        __  __  __          __    _                 
 //     / __ )____ _/ /_/ /_/ /__  _____/ /_  (_)___  _____      
@@ -10,13 +13,25 @@ def load_logo():
 
 
 def load_menu_options():
-    menu_options = """
-//  [S]tart game
+    menu_options = """//  [S]tart game
 //  [R]esume game
 //  [M]ultiplayer
 //  [E]xit
-//"""
-    print(menu_options)
+\t"""
+
+    menu_option = input(menu_options)
+    match menu_option.upper():
+        case 'S':
+            pass
+        case 'R':
+            pass
+        case 'M':
+            choose_game_mode()
+        case 'E':
+            exit()
+        case _:
+            print(wrong_option_chosen(menu_option))
+            load_menu_options()
 
 
 def choose_game_mode():
@@ -27,6 +42,20 @@ def choose_game_mode():
 //""")
 
 
+def wrong_option_chosen(option):
+    return "\tThe Option [{0}] does not exist! Try again!\n".format(option)
+
+
 if __name__ == '__main__':
+
     load_logo()
-    load_menu_options()
+    game = Game({'C': [1, 2, 3]})
+    game.update_game_board('c1')
+    game.update_game_board('c2')
+    game.update_game_board('c3')
+    game.update_game_board('j1')
+    game.update_game_board('c10')
+    game.update_game_board('h3')
+
+
+
